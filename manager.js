2ae2d2189bea8b9322ec0fdb3b13c6d5435cc4ab
fluxnas <>
2022-10-13 10:33:14 +0200

    first commit

---
+++ manager.js
@@ -0,0 +1,15 @@
+// var prompt = require("prompt-sync")();
+ 
+// let number = prompt("Welcome to your task manager, Press: \
+// 1. to see all your tasks \
+// 2. to add a task \
+// 3. to delete a task \
+// 4. to mark a task as done \
+// 5. to Exit the task manager");
+
+
+
+var prompt = require("prompt-sync")();
+ 
+let number = prompt("Welcome to your task manager, Press: 1. to see all your tasks 2. to add a task 3. to delete a task 4. to mark a task as done 5. to Exit the task manager");
+
